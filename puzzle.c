30903effc9f5a0bc4492a2d9bba0bcec22efc1df
Merot-tr <merot.music@gmail.com>
2026-04-12 10:44:35 +0300

    3x3lük alanı yazdırma

--- puzzle.c
+++ puzzle.c
@@ -4,5 +4,12 @@
 #define M 3
 int main(){
    int oyun[M][M],i,j;
+    srand(time(NULL));
+    for(i=0;i<M;i++){
+        for(j=0;j<M;j++){
+            oyun[i][j]=rand()%10;
+            printf("[%d]", oyun[i][j]);
+        }
+    }
     return 0;
 }
