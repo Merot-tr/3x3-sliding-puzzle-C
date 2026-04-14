147b53826b53fb1f2b9efa4db269d2ea1e67168b
Merot-tr <merot.music@gmail.com>
2026-04-14 17:07:06 +0300

    sayı tekrar etmesi fixlendi

--- puzzle.c
+++ puzzle.c
@@ -10,17 +10,40 @@ int main(){
         printf("%d", i);
             for(j=0;j<M;j++){
             if(i==0 && j==0 || i==0 && j==1 || i==0 && j==2 || i==1 && j==0 || i==1 && j==1 || i==1 && j==2 || i==2 && j==0 || i==2 && j==1){
-                oyun[i][j]=rand()%8+1;
-                printf("[%d]", oyun[i][j]);
-            }
-            else{do{
-            printf("[ ]");
-             oyun[i][j]=0;
+             int kontrol;
+             do{
+                 
+                 oyun[i][j]=rand()%8+1;
+                 kontrol=0;
 
-            }while(oyun[i][j]!=0);}
+                 for(int k=0;k<=i;k++){
+                   int sinir;
+                     if(k==i){
+                         sinir=j;
+                     }
+                     else{
+                         sinir=M;
+                     }
+                     for(int l=0;l<sinir;l++){
+                         if(oyun[i][j]==oyun[k][l]){
+                             kontrol=1;
+                             break;
+                         }
+                 }
+                    if(kontrol==1){
+                             break;
+                         } 
+            }}while(kontrol==1);
+                printf("[%d]",oyun[i][j]);
         }
-        printf("\n");
-   }
+        else{do{
+                printf("[ ]");
+                oyun[i][j]=0;
+
+                }while(oyun[i][j]!=0);}
+            }
+            printf("\n");
+    }
 
     return 0;
 }
