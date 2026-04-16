5fb196f03d0b6f9ed5b29b0a135c2263895481f2
hafsanur-i <hafsanuricoz@gmail.com>
2026-04-16 15:18:32 +0300

    Oyunun çözülebilirliği için geri karıştırma algoritması eklendi.
    Rastgele sayı ataması yerine, oyunun bitmiş halinden geriye doğru hamle yapılarak karıştırma sağlandı. Bu sayede oyunun her zaman çözülebilir bir dizilimle başlaması garantilendi.

--- puzzle.c
+++ puzzle.c
@@ -1,48 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 #define M 3
-int main(){
-   int oyun[M][M],i,j;
+
+int main() {
+    int oyun[M][M], i, j;
+    int bosSatir = 2, bosSutun = 2;
     srand(time(NULL));
-    printf("  0  1  2\n");
-    for(i=0;i<M;i++){
-        printf("%d", i);
-            for(j=0;j<M;j++){
-            if(i==0 && j==0 || i==0 && j==1 || i==0 && j==2 || i==1 && j==0 || i==1 && j==1 || i==1 && j==2 || i==2 && j==0 || i==2 && j==1){
-             int kontrol;
-             do{
-                 
-                 oyun[i][j]=rand()%8+1;
-                 kontrol=0;
 
-                 for(int k=0;k<=i;k++){
-                   int sinir;
-                     if(k==i){
-                         sinir=j;
-                     }
-                     else{
-                         sinir=M;
-                     }
-                     for(int l=0;l<sinir;l++){
-                         if(oyun[i][j]==oyun[k][l]){
-                             kontrol=1;
-                             break;
-                         }
-                 }
-                    if(kontrol==1){
-                             break;
-                         } 
-            }}while(kontrol==1);
-                printf("[%d]",oyun[i][j]);
-        }
-        else{do{
-                printf("[ ]");
-                oyun[i][j]=0;
-
-                }while(oyun[i][j]!=0);}
+    int sayac = 1;
+    for (i = 0; i < M; i++) {
+        for (j = 0; j < M; j++) {
+            if (i == 2 && j == 2) {
+                oyun[i][j] = 0;
+            } else {
+                oyun[i][j] = sayac++;
             }
-            printf("\n");
+        }
+    }
+
+    int hamleSayisi = 0;
+    while (hamleSayisi < 100) {
+        int yon = rand() % 4;
+        int yeniS = bosSatir, yeniK = bosSutun;
+
+        if (yon == 0) yeniS--;
+        else if (yon == 1) yeniS++;
+        else if (yon == 2) yeniK--;
+        else if (yon == 3) yeniK++;
+
+        if (yeniS >= 0 && yeniS < M && yeniK >= 0 && yeniK < M) {
+            oyun[bosSatir][bosSutun] = oyun[yeniS][yeniK];
+            oyun[yeniS][yeniK] = 0;
+            bosSatir = yeniS;
+            bosSutun = yeniK;
+            hamleSayisi++;
+        }
+    }
+
+    printf("   0  1  2\n");
+    for (i = 0; i < M; i++) {
+        printf("%d ", i);
+        for (j = 0; j < M; j++) {
+            if (oyun[i][j] == 0) {
+                printf("[ ]");
+            } else {
+                printf("[%d]", oyun[i][j]);
+            }
+        }
+        printf("\n");
     }
 
     return 0;
